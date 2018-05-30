356de0ad17560967a78f6910a8560b70d1ce942f
George Dragan <george.dragan0304@gmail.com>
2018-05-30 21:05:28 +0300

    Define the component structure, no logic implemented

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,28 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import {HeaderComponent} from "./header/header.component";
+import { IngredientComponent } from './ingredient/ingredient.component';
+import {ShoppingListComponent} from "./shopping-list/shopping-list.component";
+import { ShopingListEditComponent } from './shopping-list/shoping-list-edit/shoping-list-edit.component';
+import {RecipeItemComponent} from "./recipe/recipe-list/recipe-item/recipe-item.component";
+import {RecipeComponent} from "./recipe/recipe.component";
+import {RecipeDetailComponent} from "./recipe/recipe-detail/recipe-detail.component";
+import {RecipeListComponent} from "./recipe/recipe-list/recipe-list.component";
+
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    IngredientComponent,
+    ShoppingListComponent,
+    ShopingListEditComponent,
+    RecipeListComponent,
+    RecipeItemComponent,
+    RecipeDetailComponent,
+    RecipeComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/recipe/recipe-list/recipe-list.component.ts
@@ -0,0 +1,22 @@
+import { Component, OnInit } from '@angular/core';
+import {RecipeModel} from "../recipe.model";
+
+@Component({
+  selector: 'app-recipe-list',
+  templateUrl: './recipe-list.component.html',
+  styleUrls: ['./recipe-list.component.css']
+})
+export class RecipeListComponent implements OnInit {
+  recipes: RecipeModel[] = [
+    new RecipeModel('A Recipe', 'This is a test', 'https://www.bbcgoodfood.com/sites/default/files/recipe-collections/collection-image/2013/05/frying-pan-pizza-easy-recipe-collection.jpg'),
+    new RecipeModel('A Recipe', 'This is a test', 'https://www.bbcgoodfood.com/sites/default/files/recipe-collections/collection-image/2013/05/frying-pan-pizza-easy-recipe-collection.jpg'),
+  ];
+
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+
+}
